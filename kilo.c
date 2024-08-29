9d0842ac5f9e3ff55c517ae0a6bce25433987707
kawarimidoll <kawarimidoll+git@gmail.com>
2024-08-29 14:13:27 +1000

    feat: enable raw mode

--- kilo.c
+++ kilo.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
+#include <termios.h>
 #include <unistd.h>
+
+void enableRawMode(void) {
+  struct termios raw;
+  tcgetattr(STDIN_FILENO, &raw);
+  raw.c_lflag &= ~(ECHO);
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
 
 int main(void) {
   printf("start kilo\n");
+  enableRawMode();
+
   char c;
 
   while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
