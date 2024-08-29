fc91c56c425ca8f751fc70f8b046d2c8e69ecdf8
kawarimidoll <kawarimidoll+git@gmail.com>
2024-08-29 12:11:29 +1000

    feat: read keypress

--- kilo.c
+++ kilo.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
 
 int main(void) {
-  printf("hello world\n");
+  printf("start kilo\n");
+  char c;
+
+  while (read(STDIN_FILENO, &c, 1) == 1)
+    ;
+
+  printf("end kilo\n");
   return 0;
 }
