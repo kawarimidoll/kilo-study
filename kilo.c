65c89ce75b90a97f6d0af0c4cf5cc9c458e23c7c
kawarimidoll <kawarimidoll+git@gmail.com>
2024-08-29 15:45:02 +1000

    feat: add timeout to read()

--- kilo.c
+++ kilo.c
@@ -19,6 +19,8 @@ void enableRawMode(void) {
   raw.c_oflag &= ~(OPOST);
   raw.c_cflag |= CS8;
   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1; // 1/10 second
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
@@ -27,13 +29,17 @@ int main(void) {
   printf("start kilo\r\n");
   enableRawMode();
 
-  char c;
-
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+  while (1) {
+    char c = '\0';
+    read(STDIN_FILENO, &c, 1);
     if (iscntrl(c)) {
       printf("%d\r\n", c);
     } else {
       printf("%d ('%c')\r\n", c, c);
+    }
+    if (c == 'q') {
+      printf("quit\r\n");
+      break;
     }
   }
 
