f746578121d54bb064bf6d3934f03032de77aec8
kawarimidoll <kawarimidoll+git@gmail.com>
2024-08-29 15:07:23 +1000

    feat: add input flags and echo back what character is typed

--- kilo.c
+++ kilo.c
@@ -1,3 +1,4 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
@@ -14,7 +15,8 @@ void enableRawMode(void) {
   atexit(disableRawMode);
 
   struct termios raw = orig_termios;
-  raw.c_lflag &= ~(ECHO | ICANON);
+  raw.c_iflag &= ~(ICRNL | IXON);
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
@@ -25,8 +27,13 @@ int main(void) {
 
   char c;
 
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
-    ;
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    if (iscntrl(c)) {
+      printf("%d\n", c);
+    } else {
+      printf("%d ('%c')\n", c, c);
+    }
+  }
 
   printf("end kilo\n");
   return 0;
