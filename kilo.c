abb11890b9cfb8184180de698fadee06740e0b3c
kawarimidoll <kawarimidoll+git@gmail.com>
2024-08-29 16:15:28 +1000

    refactor: add section comments

--- kilo.c
+++ kilo.c
@@ -1,3 +1,5 @@
+/*** includes ***/
+
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
@@ -5,7 +7,11 @@
 #include <termios.h>
 #include <unistd.h>
 
+/*** data ***/
+
 struct termios orig_termios;
+
+/*** terminal ***/
 
 void die(const char* s) {
   perror(s);
@@ -36,6 +42,8 @@ void enableRawMode(void) {
     die("tcsetattr");
   }
 }
+
+/*** init ***/
 
 int main(void) {
   printf("start kilo\r\n");
