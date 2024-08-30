c1f798ade61bcc2864fe82ab8dc12bc976d31bca
kawarimidoll <kawarimidoll+git@gmail.com>
2024-08-30 14:24:51 +1000

    feat: detect window size

--- kilo.c
+++ kilo.c
@@ -4,6 +4,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
@@ -15,6 +16,8 @@
 /*** data ***/
 
 struct editorConfig {
+  int screenrows;
+  int screencols;
   struct termios orig_termios;
 };
 struct editorConfig E;
@@ -65,6 +68,16 @@ char editorReadKey(void) {
   return c;
 }
 
+int getWindowSize(int* rows, int* cols) {
+  struct winsize ws;
+  if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+    return -1;
+  }
+  *cols = ws.ws_col;
+  *rows = ws.ws_row;
+  return 0;
+}
+
 /*** output ***/
 
 void editorRefreshScreen(void) {
@@ -88,8 +101,15 @@ int editorProcessKeypress(void) {
 
 /*** init ***/
 
+void initEditor(void) {
+  if (getWindowSize(&E.screenrows, &E.screencols) == -1) {
+    die("getWindowSize");
+  }
+}
+
 int main(void) {
   printf("start kilo\r\n");
+  initEditor();
   enableRawMode();
 
   while (1) {
