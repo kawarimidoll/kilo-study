a00522956d2dd513645ff63af79d1c461f409797
kawarimidoll <kawarimidoll+git@gmail.com>
2024-08-30 15:02:47 +1000

    feat: add getCursorPosition to fallback checking screen size

--- kilo.c
+++ kilo.c
@@ -68,10 +68,48 @@ char editorReadKey(void) {
   return c;
 }
 
+int getCursorPosition(int* rows, int* cols) {
+  char buf[32];
+  unsigned int i = 0;
+
+  if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) {
+    return -1;
+  }
+
+  while (i < sizeof(buf) - 1) {
+    if (read(STDIN_FILENO, &buf[i], 1) != 1) {
+      break;
+    }
+    if (buf[i] == 'R') {
+      break;
+    }
+    i++;
+  }
+  buf[i] = '\0';
+
+  /* make sure it responded with an escape sequence */
+  if (buf[0] != '\x1b' || buf[1] != '[') {
+    return -1;
+  }
+  if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) {
+    return -1;
+  }
+
+  /* to check */
+  /* printf("%d, %d \r\n", *rows, *cols); */
+  /* printf("hoge \r\n"); */
+  /* editorReadKey(); */
+
+  return 0;
+}
+
 int getWindowSize(int* rows, int* cols) {
   struct winsize ws;
   if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
-    return -1;
+    if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) {
+      return -1;
+    }
+    return getCursorPosition(rows, cols);
   }
   *cols = ws.ws_col;
   *rows = ws.ws_row;
