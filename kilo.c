fb517557587f07b3b21fd3fc913ea3fe8f2d3b73
kawarimidoll <kawarimidoll+git@gmail.com>
2024-09-01 20:25:39 +1000

    feat: render tab character

--- kilo.c
+++ kilo.c
@@ -17,6 +17,7 @@
 /*** defines ***/
 
 #define KILO_VERSION "0.0.1"
+#define KILO_TAB_STOP 8
 
 // 0x1f represents 00011111
 #define CTRL_KEY(k) ((k) & 0x1f)
@@ -37,7 +38,9 @@ enum editorKey {
 
 typedef struct erow {
   int size;
+  int rsize;
   char* chars;
+  char* render;
 } erow;
 
 struct editorConfig {
@@ -200,6 +203,33 @@ int getWindowSize(int* rows, int* cols) {
 
 /*** row operations ***/
 
+void editorUpdateRow(erow* row) {
+  int j;
+  int tabs = 0;
+  for (j = 0; j < row->size; j++) {
+    if (row->chars[j] == '\t') {
+      tabs++;
+    }
+  }
+
+  free(row->render);
+  row->render = malloc(row->size + tabs * (KILO_TAB_STOP - 1) + 1);
+
+  int idx = 0;
+  for (j = 0; j < row->size; j++) {
+    if (row->chars[j] == '\t') {
+      row->render[idx++] = ' ';
+      while (idx % KILO_TAB_STOP != 0) {
+        row->render[idx++] = ' ';
+      }
+    } else {
+      row->render[idx++] = row->chars[j];
+    }
+  }
+  row->render[idx] = '\0';
+  row->rsize = idx;
+}
+
 void editorAppendRow(char* s, size_t len) {
   E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
 
@@ -208,6 +238,11 @@ void editorAppendRow(char* s, size_t len) {
   E.row[at].chars = malloc(len + 1);
   memcpy(E.row[at].chars, s, len);
   E.row[at].chars[len] = '\0';
+
+  E.row[at].rsize = 0;
+  E.row[at].render = NULL;
+  editorUpdateRow(&E.row[at]);
+
   E.numrows++;
 }
 
@@ -302,14 +337,14 @@ void editorDrawRows(struct abuf* ab) {
         abAppend(ab, "~", 1);
       }
     } else {
-      int len = E.row[filerow].size - E.coloff;
+      int len = E.row[filerow].rsize - E.coloff;
       if (len < 0) {
         len = 0;
       }
       if (len > E.screencols) {
         len = E.screencols;
       }
-      abAppend(ab, &E.row[filerow].chars[E.coloff], len);
+      abAppend(ab, &E.row[filerow].render[E.coloff], len);
     }
     /* <esc>K to clear the right part of the current line */
     abAppend(ab, "\x1b[K\r\n", 5);
