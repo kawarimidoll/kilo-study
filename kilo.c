d51480db64d8d0d9fd685282464bcd62010e6e52
kawarimidoll <kawarimidoll+git@gmail.com>
2024-09-01 13:30:34 +1000

    feat: read file

--- kilo.c
+++ kilo.c
@@ -1,5 +1,9 @@
 /*** includes ***/
 
+#define _DEFAULT_SOURCE
+#define _BSD_SOURCE
+#define _GNU_SOURCE
+
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
@@ -195,15 +199,30 @@ int getWindowSize(int* rows, int* cols) {
 
 /*** file io ***/
 
-void editorOpen(void) {
-  char* line = "Hello,world!";
-  ssize_t linelen = 13;
+void editorOpen(char* filename) {
+  FILE* fp = fopen(filename, "r");
+  if (!fp) {
+    die("fopen");
+  }
 
-  E.row.size = linelen;
-  E.row.chars = malloc(linelen + 1);
-  memcpy(E.row.chars, line, linelen);
-  E.row.chars[linelen] = '\0';
-  E.numrows = 1;
+  char* line = NULL;
+  size_t linecap = 0;
+  ssize_t linelen = getline(&line, &linecap, fp);
+
+  if (linelen != -1) {
+    while (linelen > 0 &&
+           (line[linelen - 1] == '\n' || line[linelen - 1] == '\r')) {
+      linelen--;
+    }
+
+    E.row.size = linelen;
+    E.row.chars = malloc(linelen + 1);
+    memcpy(E.row.chars, line, linelen);
+    E.row.chars[linelen] = '\0';
+    E.numrows = 1;
+  }
+  free(line);
+  fclose(fp);
 }
 
 /*** append buffer ***/
@@ -362,11 +381,13 @@ void initEditor(void) {
   }
 }
 
-int main(void) {
+int main(int argc, char* argv[]) {
   printf("start kilo\r\n");
   enableRawMode();
   initEditor();
-  editorOpen();
+  if (argc >= 2) {
+    editorOpen(argv[1]);
+  }
 
   while (1) {
     editorRefreshScreen();
