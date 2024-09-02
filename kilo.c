2f1b877860870cad2f6d039f8a082042971ad1ca
kawarimidoll <kawarimidoll+git@gmail.com>
2024-09-02 21:50:03 +1000

    feat: show modified flag

--- kilo.c
+++ kilo.c
@@ -54,6 +54,7 @@ struct editorConfig {
   int screenrows, screencols;
   struct termios orig_termios;
   int numrows;
+  int dirty;
   char* filename;
   char statusmsg[80];
   time_t statusmsg_time;
@@ -269,6 +270,7 @@ void editorAppendRow(char* s, size_t len) {
   editorUpdateRow(&E.row[at]);
 
   E.numrows++;
+  E.dirty++;
 }
 
 void editorRowInsertChar(erow* row, int at, int c) {
@@ -281,6 +283,7 @@ void editorRowInsertChar(erow* row, int at, int c) {
   row->size++;
   row->chars[at] = c;
   editorUpdateRow(row);
+  E.dirty++;
 }
 
 /*** editor operations ***/
@@ -339,6 +342,7 @@ void editorOpen(char* filename) {
   }
   free(line);
   fclose(fp);
+  E.dirty = 0;
 }
 
 void editorSave(void) {
@@ -356,6 +360,7 @@ void editorSave(void) {
         close(fd);
         free(buf);
         editorSetStatusMessage("%d bytes written to disk", len);
+        E.dirty = 0;
         return;
       }
     }
@@ -456,8 +461,9 @@ void editorDrawRows(struct abuf* ab) {
 void editorDrawStatusBar(struct abuf* ab) {
   abAppend(ab, "\x1b[7m", 4);
   char status[80], rstatus[80];
-  int len = snprintf(status, sizeof(status), "%.20s - %d lines",
-                     E.filename ? E.filename : "[No Name]", E.numrows);
+  int len = snprintf(status, sizeof(status), "%.20s - %d lines %s",
+                     E.filename ? E.filename : "[No Name]", E.numrows,
+                     E.dirty ? "(modified)" : "");
   /* add 1 since E.cy is 0-indexed */
   int rlen = snprintf(rstatus, sizeof(rstatus), "%d/%d", E.cy + 1, E.numrows);
   if (len > E.screencols) {
@@ -659,6 +665,7 @@ void initEditor(void) {
   E.rowoff = 0;
   E.coloff = 0;
   E.numrows = 0;
+  E.dirty = 0;
   E.row = NULL;
   E.filename = NULL;
   E.statusmsg[0] = '\0';
@@ -677,7 +684,8 @@ int main(int argc, char* argv[]) {
   if (argc >= 2) {
     editorOpen(argv[1]);
   }
-  editorSetStatusMessage("HELP: ctrl-q = quit, ctrl-s = save, arrow keys = move");
+  editorSetStatusMessage(
+      "HELP: ctrl-q = quit, ctrl-s = save, arrow keys = move");
 
   while (1) {
     editorRefreshScreen();
