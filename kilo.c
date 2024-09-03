a8a8405ddaa825fe791849e0ed9847a10083def8
kawarimidoll <kawarimidoll+git@gmail.com>
2024-09-03 13:56:00 +1000

    feat: implement newline

--- kilo.c
+++ kilo.c
@@ -257,10 +257,14 @@ void editorUpdateRow(erow* row) {
   row->rsize = idx;
 }
 
-void editorAppendRow(char* s, size_t len) {
-  E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+void editorInsertRow(int at, char* s, size_t len) {
+  if (at < 0 || at > E.numrows) {
+    return;
+  }
 
-  int at = E.numrows;
+  E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+  memmove(&E.row[at + 1], &E.row[at], sizeof(erow) * (E.numrows - at));
+
   E.row[at].size = len;
   E.row[at].chars = malloc(len + 1);
   memcpy(E.row[at].chars, s, len);
@@ -328,10 +332,25 @@ void editorRowDelChar(erow* row, int at) {
 void editorInsertChar(int c) {
   if (E.cy == E.numrows) {
     /* append line */
-    editorAppendRow("", 0);
+    editorInsertRow(E.numrows, "", 0);
   }
   editorRowInsertChar(&E.row[E.cy], E.cx, c);
   E.cx++;
+}
+
+void editorInsertNewline(void) {
+  if (E.cx == 0) {
+    editorInsertRow(E.cy, "", 0);
+  } else {
+    erow* row = &E.row[E.cy];
+    editorInsertRow(E.cy + 1, &row->chars[E.cx], row->size - E.cx);
+    row = &E.row[E.cy];
+    row->size = E.cx;
+    row->chars[row->size] = '\0';
+    editorUpdateRow(row);
+  }
+  E.cy++;
+  E.cx = 0;
 }
 
 void editorDelChar(void) {
@@ -393,7 +412,7 @@ void editorOpen(char* filename) {
       linelen--;
     }
 
-    editorAppendRow(line, linelen);
+    editorInsertRow(E.numrows, line, linelen);
   }
   free(line);
   fclose(fp);
@@ -632,7 +651,7 @@ int editorProcessKeypress(void) {
   int c = editorReadKey();
   switch (c) {
     case '\r':
-      /* TODO: insert new line */
+      editorInsertNewline();
       break;
 
     /* case 'q': */
