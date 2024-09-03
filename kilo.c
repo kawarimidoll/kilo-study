2ce1c43e5014c2551858515bd8ee1f57b3eba868
kawarimidoll <kawarimidoll+git@gmail.com>
2024-09-03 16:49:06 +1000

    feat: highlight decimal point as a number

--- kilo.c
+++ kilo.c
@@ -240,7 +240,8 @@ void editorUpdateSyntax(erow* row) {
     char c = row->render[i];
     unsigned char prev_hl = (i > 0) ? row->hl[i - 1] : HL_NORMAL;
 
-    if (isdigit(c) && (prev_sep || prev_hl == HL_NUMBER)) {
+    if ((isdigit(c) && (prev_sep || prev_hl == HL_NUMBER)) ||
+        (prev_hl == HL_NUMBER && c == '.')) {
       row->hl[i] = HL_NUMBER;
       prev_sep = 0;
       i++;
