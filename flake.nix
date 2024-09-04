a774761032df1dd64e22dcd5740cf4917a2e63c2
kawarimidoll <kawarimidoll+git@gmail.com>
2024-09-04 20:22:43 +1000

    feat: build using nix

--- flake.nix
+++ flake.nix
@@ -1,20 +1,13 @@
 {
   description = "kilo-study development environment";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
+  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
   outputs = {
     self,
     nixpkgs,
   }: let
-    supportedSystems = [
-      "x86_64-linux"
-      "aarch64-linux"
-      "x86_64-darwin"
-      "aarch64-darwin"
-    ];
+    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
     forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
   in {
     devShells = forAllSystems (system: let
@@ -31,6 +24,24 @@
           echo "make"
           which make
           make --version
+        '';
+      };
+    });
+
+    packages = forAllSystems (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      default = pkgs.stdenv.mkDerivation {
+        name = "kilo-study";
+        src = ./.;
+        buildInputs = with pkgs; [gcc];
+        buildPhase = ''
+          gcc kilo.c -o kilo -Wall -Wextra -pedantic -std=c99
+        '';
+        installPhase = ''
+          install -D -t $out/bin kilo
         '';
       };
     });
