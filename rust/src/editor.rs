56ab17258146af0ed3d9797efbd5ef5243dac3e2
kawarimidoll <kawarimidoll+git@gmail.com>
2024-09-05 16:57:17 +1000

    feat: use crossterm::event

--- rust/src/editor.rs
+++ rust/src/editor.rs
@@ -1,5 +1,5 @@
+use crossterm::event::{read, Event::Key, KeyCode::Char};
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
-use std::io::{self, Read};
 
 pub struct Editor {}
 
@@ -12,20 +12,21 @@ impl Editor {
         println!("kilo start");
         enable_raw_mode().unwrap();
 
-        for b in io::stdin().bytes() {
-            match b {
-                Ok(b) => {
-                    let c = b as char;
-                    if c.is_control() {
-                        println!("Binary: {0:08b} ASCII: {0:#03} \r", b);
-                    } else {
-                        println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r", b, c);
-                    }
-                    if c == 'q' {
-                        break;
+        loop {
+            match read() {
+                Ok(Key(event)) => {
+                    println!("{:?}\r", event);
+                    match event.code {
+                        Char(c) => {
+                            if c == 'q' {
+                                break;
+                            }
+                        }
+                        _ => (),
                     }
                 }
                 Err(err) => println!("{}", err),
+                _ => (),
             }
         }
 
