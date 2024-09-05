67975bba0bfa1677e34328b0b0a30063cd92c30d
kawarimidoll <kawarimidoll+git@gmail.com>
2024-09-05 16:07:08 +1000

    feat: use match to handle errors

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -6,18 +6,22 @@ fn main() {
     enable_raw_mode().unwrap();
 
     for b in io::stdin().bytes() {
-        let b = b.unwrap();
-        let c = b as char;
-        if c.is_control() {
-            println!("Binary: {0:08b} ASCII: {0:#03} \r", b);
-        } else {
-            println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r", b, c);
-        }
-        if c == 'q' {
-            disable_raw_mode().unwrap();
-            break;
+        match b {
+            Ok(b) => {
+                let c = b as char;
+                if c.is_control() {
+                    println!("Binary: {0:08b} ASCII: {0:#03} \r", b);
+                } else {
+                    println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r", b, c);
+                }
+                if c == 'q' {
+                    break;
+                }
+            }
+            Err(err) => println!("{}", err),
         }
     }
 
+    disable_raw_mode().unwrap();
     println!("kilo end");
 }
