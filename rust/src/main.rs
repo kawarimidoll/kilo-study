a862142687013c1ee085336d94edf3f78059e15c
kawarimidoll <kawarimidoll+git@gmail.com>
2024-09-05 15:34:43 +1000

    feat: read character in raw mode

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,12 +1,20 @@
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
 use std::io::{self, Read};
 
 fn main() {
     println!("kilo start");
+    enable_raw_mode().unwrap();
 
     for b in io::stdin().bytes() {
-        let c = b.unwrap() as char;
-        println!("{}", c);
+        let b = b.unwrap();
+        let c = b as char;
+        if c.is_control() {
+            println!("Binary: {0:08b} ASCII: {0:#03} \r", b);
+        } else {
+            println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r", b, c);
+        }
         if c == 'q' {
+            disable_raw_mode().unwrap();
             break;
         }
     }
