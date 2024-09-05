e1ea359e5ad6f605562107f816ea6040ccd8dda5
kawarimidoll <kawarimidoll+git@gmail.com>
2024-09-06 09:38:54 +1000

    refactor: use boolean flag to quit loop

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,8 +3,5 @@ mod editor;
 use editor::Editor;
 
 fn main() {
-    let editor = Editor::default();
-    editor.run();
-    // above is same as below
-    // Editor::run(&editor);
+    Editor::default().run();
 }
