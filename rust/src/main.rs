77265132fb203098a249e23b87ea2aa6a375ee2a
kawarimidoll <kawarimidoll+git@gmail.com>
2024-09-05 14:29:34 +1000

    feat: get user input

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,15 @@
+use std::io::{self, Read};
+
 fn main() {
-    println!("Hello, world!");
+    println!("kilo start");
+
+    for b in io::stdin().bytes() {
+        let c = b.unwrap() as char;
+        println!("{}", c);
+        if c == 'q' {
+            break;
+        }
+    }
+
+    println!("kilo end");
 }
