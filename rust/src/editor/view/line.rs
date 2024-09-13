d054c17bfaacfc5b35abd6cebeb7b68f08e14453
kawarimidoll <kawarimidoll+git@gmail.com>
2024-09-13 14:00:31 +1000

    feat: update line to handle graphemes

--- rust/src/editor/view/line.rs
+++ rust/src/editor/view/line.rs
@@ -1,26 +1,88 @@
-use std::{cmp, ops::Range};
+use std::ops::Range;
 use unicode_segmentation::UnicodeSegmentation;
+use unicode_width::UnicodeWidthStr;
+
+enum GraphemeWidth {
+    Half,
+    Full,
+}
+
+impl GraphemeWidth {
+    fn saturating_add(&self, other: usize) -> usize {
+        match self {
+            Self::Half => other.saturating_add(1),
+            Self::Full => other.saturating_add(2),
+        }
+    }
+}
+
+struct TextFragment {
+    grapheme: String,
+    width: GraphemeWidth,
+    replacement: Option<char>,
+}
+
+impl TextFragment {
+    pub fn new(grapheme: &str) -> Self {
+        let width = match grapheme.width() {
+            0 | 1 => GraphemeWidth::Half,
+            _ => GraphemeWidth::Full,
+        };
+        let replacement = match grapheme.width() {
+            0 => Some('·'),
+            _ => None,
+        };
+        Self {
+            grapheme: String::from(grapheme),
+            width,
+            replacement,
+        }
+    }
+}
 
 pub struct Line {
-    string: String,
+    fragments: Vec<TextFragment>,
 }
 
 impl Line {
     pub fn from(string: &str) -> Self {
         Self {
-            string: String::from(string),
+            fragments: string.graphemes(true).map(TextFragment::new).collect(),
         }
     }
     pub fn get(&self, range: Range<usize>) -> String {
+        let mut result = String::new();
         let start = range.start;
-        let end = cmp::min(range.end, self.len());
-        self.string
-            .graphemes(true)
-            .skip(start)
-            .take(end.saturating_sub(start))
-            .collect()
+        let end = range.end;
+
+        if start >= end {
+            return result;
+        }
+        let mut current_pos = 0;
+
+        for fragment in &self.fragments {
+            if current_pos >= end {
+                break;
+            }
+            let fragment_end = fragment.width.saturating_add(current_pos);
+            if fragment_end >= start {
+                if fragment_end >= end || current_pos < start {
+                    // boundary of screen
+                    result.push('…');
+                } else if let Some(char) = fragment.replacement {
+                    // use replacement character for empty graphemes
+                    result.push(char);
+                } else {
+                    // use the original grapheme
+                    result.push_str(&fragment.grapheme);
+                }
+            }
+            current_pos = fragment_end
+        }
+
+        result
     }
     pub fn len(&self) -> usize {
-        self.string.graphemes(true).count()
+        self.fragments.len()
     }
 }
