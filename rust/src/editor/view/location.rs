da6701c9e25931dbd4d06ef887fa7d4ecf694232
kawarimidoll <kawarimidoll+git@gmail.com>
2024-09-13 14:57:26 +1000

    fix: remove unused parts from location.ls

--- rust/src/editor/view/location.rs
+++ rust/src/editor/view/location.rs
@@ -1,26 +1,6 @@
-use crate::editor::terminal::Position;
-
 #[derive(Copy, Clone, Default)]
 pub struct Location {
     // the position of the document
     pub x: usize,
     pub y: usize,
 }
-impl From<Location> for Position {
-    fn from(location: Location) -> Self {
-        Self {
-            col: location.x,
-            row: location.y,
-        }
-    }
-}
-impl Location {
-    pub const fn subtract(&self, other: &Self) -> Self {
-        Self {
-            x: self.x.saturating_sub(other.x),
-            y: self.y.saturating_sub(other.y),
-        }
-    }
-}
-
-
